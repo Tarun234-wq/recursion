8e0a41539bc7c56f9e138aa91939be4d227ed03e
Tarun Guleria <81820814+Tarun234-wq@users.noreply.github.com>
2022-10-24 03:36:14 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@recursionl/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int t;
+  cout << "Enter testcase:" << endl;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+  
+   
+  }
+  return 0;
+}
